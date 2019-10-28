7e0836eacfa54f3eeaeafb52b206f9c6b9572056
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-10-28 09:34:42 +0100

    Initial project setup.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+
+
