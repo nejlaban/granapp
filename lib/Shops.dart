fd84a6740759ea0b068ed3d82246b3ebefd650dd
nejlaban <nejla.bandic@stu.ibu.edu.ba>
2021-07-02 10:54:17 +0200

    Added Homepage screen,Login screen,Bottom Nav. Bar

---
+++ lib/Shops.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Shops extends StatefulWidget {
+  @override
+  _ShopsState createState() => _ShopsState();
+}
+
+class _ShopsState extends State<Shops> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
